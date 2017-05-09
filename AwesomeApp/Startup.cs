a8edf05643a2978bc2a9645f90872712618be38f
aminisonya <aminisonya@gmail.com>
2017-05-08 19:10:58 -0700

    Initial commit
    first commit after project set up

---
+++ AwesomeApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AwesomeApp.Startup))]
+namespace AwesomeApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
